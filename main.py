9e42145e3d71a0a0fa1abeb02f044494240e64bf
kankan <kankankalita223@gmail.com>
2026-08-12 17:30:25 +0530

    This commit is done on 2026-08-12 17:30:25

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ from datetime import datetime
 now = datetime.now()
 
 os.system("git add .")
-os.system(f"git commit -m \"this commit is done on {now.strftime("%Y-%m-%d %H:%M:%S")}\"")
-# os.system("git add .")
+os.system(f"git commit -m \"This commit is done on {now.strftime("%Y-%m-%d %H:%M:%S")}\"")
+os.system("git push -u origin main")
