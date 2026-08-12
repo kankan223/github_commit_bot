ac9d82b3620beb1bd06d6cf83c37ff07092317e6
kankan <kankankalita223@gmail.com>
2026-08-12 18:32:27 +0530

    This commit is done on 2026-08-12 18:32:27

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-# exec(open('script.sh').read())   
-
 import os
 from datetime import datetime
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 now = datetime.now()
-path = Path.cwd()
+path = os.getenv("path")
 
-os.system(f"cd {path}")
+os.chdir(path)
+
+
 
 os.system("git add .")
 os.system(f"git commit -m \"This commit is done on {now.strftime("%Y-%m-%d %H:%M:%S")}\"")
