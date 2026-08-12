4100f7dc4c04d6fe7c695305473de9728eaf8ff9
kankan <kankankalita223@gmail.com>
2026-08-12 17:29:45 +0530

    this commit is done on 2026-08-12 17:29:45

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # exec(open('script.sh').read())   
 
 import os
-import datetime
+from datetime import datetime
+
+now = datetime.now()
 
 os.system("git add .")
-os.system(f"git commit -m \"this commit is done on {datetime.datetime}\"")
+os.system(f"git commit -m \"this commit is done on {now.strftime("%Y-%m-%d %H:%M:%S")}\"")
 # os.system("git add .")
