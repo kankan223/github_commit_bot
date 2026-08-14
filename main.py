b8b337e01c31d9ebb88326faa2f16f7b673f4251
kankan <kankankalita223@gmail.com>
2026-08-14 18:21:50 +0530

    This commit is done on 2026-08-14 18:21:50

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import os
 from datetime import datetime
-from pathlib import Path
+# from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
 
 now = datetime.now()
-path = os.getenv("path")
+path = os.getenv("repo_path")
 
 os.chdir(path)
 
 
 
 os.system("git add .")
-os.system(f"git commit -m \"This commit is done on {now.strftime("%Y-%m-%d %H:%M:%S")}\"")
+os.system(f"git commit -m \"This commit is done on {now.strftime('%Y-%m-%d %H:%M:%S')}\"")
 os.system("git push -u origin main")
