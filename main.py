025fa065e8ac3c98038d565e318a00a926b315b9
kankan <kankankalita223@gmail.com>
2026-08-15 23:02:51 +0530

    This commit is done on 2026-08-15 23:02:51

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ path = Path(os.getenv("repo_path"))
 
 os.chdir(path)
 
+os.makedirs("log", exist_ok=True)
+with open("log/logs.txt", "a") as f:
+    f.write(f"A new commit done on {now.strftime('%Y-%m-%d %H:%M:%S')}\n\n")
+
 
 
 os.system("git add .")
